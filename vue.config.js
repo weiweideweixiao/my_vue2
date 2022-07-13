6c60e51bbc416547eaa18fa578c1938ccf489119
Xuemei.Rui <ruixuemei@peerfintech.cn>
2022-07-13 10:20:24 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,6 @@
-// const path = require('path');
+const path = require('path');
 
 
-// module.exports = {
-//     entry: './src/main.js', // 入口文件
-//     output: { // 输出配置
-//         filename: 'built.js', // 输出文件名
-//         path: path.resolve(__dirname, 'dist_my_vue2') // 输出文件路径配置
-//     },
-// };
+module.exports = {
+    outputDir: "my_vue2_dist",
+};
